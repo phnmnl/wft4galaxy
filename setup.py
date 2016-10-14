64fde32f29961fcb74ac3a1493407302abaf22d5
Marco Enrico Piras <kikkomep@crs4.it>
2016-10-14 15:40:55 +0200

    Added initial setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='workflow_tester',
+      description='Utility package for testing Galaxy workflows',
+      author='CRS4',
+      url='https://bitbucket.org/kikkomep/workflowtester/',
+      py_modules=['workflow_tester'],
+      )
