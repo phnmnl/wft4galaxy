98bcdf0f52da8a5d63d41b6552393173a6db5af6
Marco Enrico Piras <kikkomep@crs4.it>
2016-11-03 17:26:46 +0100

    Fixed requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
 
-setup(name='wft4galaxy',
-      description='Utility package for testing Galaxy workflows',
-      author='CRS4',
-      url='https://bitbucket.org/crs4/wft4galaxy/',
-      py_modules=['wft4galaxy'],
-      entry_points={'console_scripts': ['wft4galaxy = wft4galaxy:run_tests']},
-      )
+setup(
+    name='wft4galaxy',
+    description='Utility module for testing Galaxy workflows',
+    url='https://github.com/phnmnl/wft4galaxy',
+    version='0.1',
+    install_requires={
+        'setuptools': ['setuptools'],
+        'bioblend': ['bioblend>=0.8.0'],
+        'ruamel.yaml': ['ruamel.yaml'],
+        'lxml': ['lxml'],
+        'pyyaml': ['pyyaml'],
+        'sphinx_rtd_theme': ['sphinx_rtd_theme']
+    },
+    py_modules=['wft4galaxy'],
+    entry_points={'console_scripts': ['wft4galaxy = wft4galaxy:run_tests']},
+)
