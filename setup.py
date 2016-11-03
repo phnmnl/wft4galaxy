212db3ef54820d7ab1388d3d9d0c9ee2dce38e65
Marco Enrico Piras <kikkomep@crs4.it>
2016-11-03 11:11:01 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='workflow_tester',
+setup(name='wft4galaxy',
       description='Utility package for testing Galaxy workflows',
       author='CRS4',
-      url='https://bitbucket.org/kikkomep/workflowtester/',
-      py_modules=['workflow_tester'],
+      url='https://bitbucket.org/crs4/wft4galaxy/',
+      py_modules=['wft4galaxy'],
+      entry_points={'console_scripts': ['wft4galaxy = wft4galaxy:run_tests']},
       )
