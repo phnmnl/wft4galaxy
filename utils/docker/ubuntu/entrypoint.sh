a55338ca2d9fee81689789984bce63738892e8da
Marco Enrico Piras <kikkomep@crs4.it>
2016-11-04 10:56:00 +0100

    Updated image based on ubuntu to include jupyter

--- utils/docker/ubuntu/entrypoint.sh
+++ utils/docker/ubuntu/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-if [[ ${1} == "bash" ]]; then
+if [[ ${1} == "wft4galaxy" ]]; then
     shift
-    /bin/bash "$@"
-else
     wft4galaxy "$@"
+elif [[ ${1} == "ipython" ]]; then
+		ipython
+elif [[ ${1} == "jupyter" ]]; then
+		shift
+		ipython notebook --ip=$(hostname) --no-browser --port 8888 "$@"
+else
+	/bin/bash "$@"
 fi
