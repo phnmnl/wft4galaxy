8befe8fc99c7ab602fa43e6dc848cf920ff1278b
Marco Enrico Piras <kikkomep@crs4.it>
2017-03-09 17:50:29 +0100

    Add utility class which implements a Dynamic object

--- wft4galaxy/common.py
+++ wft4galaxy/common.py
@@ -1,4 +1,5 @@
 import os as _os
+import types as _types
 
 # Galaxy ENV variable names
 ENV_KEY_GALAXY_URL = "GALAXY_URL"
@@ -6,4 +7,23 @@ ENV_KEY_GALAXY_API_KEY = "GALAXY_API_KEY"
 
 
 class TestConfigError(RuntimeError):
+    pass
+
+
+class DynamicObject(dict):
+    """ Represents a dynamic object  """
+
+    def __init__(self, initial_properties=None):
+        self.__dict__ = self
+        if initial_properties:
+            self.update(initial_properties)
+
+    def __setattr__(self, name, value):
+        if isinstance(value, _types.FunctionType):
+            self[name] = _types.MethodType(value, self)
+        else:
+            super(DynamicObject, self).__setattr__(name, value)
+
+
+class Configuration(DynamicObject):
     pass
