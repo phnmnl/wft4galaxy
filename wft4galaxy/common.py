fb53fd8e1d513e9314f13403293497aad1b7b31e
Marco Enrico Piras <kikkomep@crs4.it>
2017-03-15 15:29:13 +0100

    Add initial logic to extract a workflow from a history

--- wft4galaxy/common.py
+++ wft4galaxy/common.py
@@ -85,6 +85,168 @@ class HistoryWrapper(object):
             elif hd.id not in self.input_datasets:
                 print("Intermediary output %r" % hd.id)
                 self.intermediate_outputs[hd.id] = hd
+
+    def extract_workflow(self, filename=None):
+        wf = _collections.OrderedDict({
+            "a_galaxy_workflow": "true",
+            "annotation": "",
+            "format-version": "0.1",
+            "name": "Imported Workflow",
+            "uuid": str(_uuid.uuid1()),
+            "steps": _collections.OrderedDict()
+        })
+
+        # processed jobs
+        processed_jobs = []
+
+        # map hds to step
+        hds_map = {}
+
+        # position
+        p_left = 50
+        p_top = 0
+
+        # map output id to its name
+        output_labels = {}
+
+        # process steps
+        for hds_id, hds in self.input_datasets.items():
+            job = self.creating_jobs[hds.id]
+            index = len(wf["steps"])
+            hds_map[hds_id] = index
+            input_name = "input_{0}".format(index)
+            input_description = ""
+            p_top += 100
+            wf["steps"][str(index)] = {
+                "annotation": "",
+                "content_id": None,
+                "id": index,
+                "input_connections": {},
+                "inputs": [
+                    {
+                        "description": input_description,
+                        "name": input_name
+                    }
+                ],
+                "label": None,
+                "name": "Input dataset",
+                "outputs": [],
+                "position": {
+                    "left": p_left,
+                    "top": p_top
+                },
+                "tool_errors": None,
+                "tool_id": None,
+                "tool_state": _json.dumps({"name": input_name}),
+                "tool_version": None,
+                "type": "data_input",
+                "uuid": str(_uuid.uuid1()),
+                "workflow_outputs": []
+            }
+            processed_jobs.append(job.id)
+            print processed_jobs
+
+        p_top = p_top - (100 * (len(self.input_datasets) / 2))
+
+        # process intermediate and final steps
+        for hds_id, hds in self.intermediate_outputs.items() + self.output_datasets.items():
+
+            job = self.creating_jobs[hds.id]
+            if job.id in processed_jobs:
+                continue
+            tool = self.job_tool[hds_id]
+
+            p_left += 200
+
+            print "Processing {0} node: {1}".format("intermediate" if hds_id in self.intermediate_outputs else "output",
+                                                    tool.name)
+
+            # compute params
+            params = {"__page__": 0, "__rerun_remap_job_id__": None}
+            tool_inputs = {param["name"]: param for param in tool.wrapped["inputs"]}
+            for param_name, param_info in job.wrapped["params"].items():
+                print "Params", param_name
+                if param_name in tool_inputs:
+                    params[param_name] = param_info
+
+            # add inputs to tool state and inputs
+            inputs = []
+            input_connections = {}
+            for job_input_name, job_input_info in job.wrapped["inputs"].items():
+                print "Input", job_input_name
+                params[job_input_name] = _json.dumps({"__class__": "RuntimeValue"})
+                inputs.append({
+                    "description": "Runtime input value {0}".format(job_input_name),
+                    "name": job_input_name
+                })
+                input_connections[job_input_name] = {
+                    "id": hds_map[job_input_info["id"]],
+                    "output_name": output_labels[job_input_info["id"]] if job_input_info[
+                                                                              "id"] in output_labels else "output"
+                }
+
+            # add outputs
+            outputs = []
+            workflow_outputs = []
+            tool_outputs = {param["name"]: param for param in tool.wrapped["outputs"]}
+            for job_output_name, job_output_info in job.wrapped["outputs"].items():
+                outputs.append({
+                    "name": job_output_name,
+                    "type": tool_outputs[job_output_name]["format"]
+                })
+                workflow_outputs.append({
+                    "label": job_output_name,
+                    "output_name": job_output_name,
+                    "uuid": str(_uuid.uuid1())
+                })
+
+                output_labels[job_output_info["id"]] = job_output_name
+
+            index = len(wf["steps"])
+            hds_map[hds_id] = index
+
+            wf["steps"][str(index)] = {
+                "annotation": "",
+                "content_id": tool.name,
+                "id": index,
+                "input_connections": input_connections,
+                "inputs": inputs,
+                "label": None,
+                "name": tool.name,
+                "outputs": outputs,
+                "position": {
+                    "left": p_left,
+                    "top": p_top
+                },
+                "tool_errors": None,
+                "tool_id": tool.name,
+                "tool_state": _json.dumps(params),
+                "tool_version": tool.version,
+                "type": "tool",
+                "uuid": str(_uuid.uuid1()),
+                "workflow_outputs": workflow_outputs
+            }
+            print tool.name, workflow_outputs
+            processed_jobs.append(job.id)
+            print processed_jobs
+
+        # TODO: move me!!!!
+        self.dataset_index = hds_map
+        self.output_labels = output_labels
+
+        # save workflow
+        print "Filename", filename
+        with open(filename, "w") as fp:
+            _json.dump(wf, fp, indent=4)
+
+        return wf
+
+
+def extract_workflow(history, filename=None):
+    hw = HistoryWrapper(history)
+    return hw.extract_workflow(filename)
+
+
 def _pformat(obj):
     return _json.dumps(obj, sort_keys=True, indent=4)
 
