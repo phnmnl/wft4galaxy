b10ecca059c1ab56671de428ab94aebea2a08729
Marco Enrico Piras <kikkomep@crs4.it>
2017-03-22 17:18:25 +0100

    Update modules exposed from packages

--- wft4galaxy/__init__.py
+++ wft4galaxy/__init__.py
@@ -1,3 +1,4 @@
-from wft4galaxy import common
-from wft4galaxy import core
 from wft4galaxy import app
+from wft4galaxy import core
+from wft4galaxy import common
+from wft4galaxy import wrapper
---
+++ wft4galaxy/app/__init__.py
@@ -0,0 +1,3 @@
+from . import runner
+from . import wizard
+from . import docker_runner
