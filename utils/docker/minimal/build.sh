045cd4e7c5deacbc2ab972d318cefa1ac9d0f814
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-24 10:04:02 +0200

    Rename and generalize the script for building Docker image (minimal version)

--- utils/docker/minimal/build.sh
+++ utils/docker/minimal/build.sh
@@ -1,26 +1,20 @@
 #!/usr/bin/env bash
 
+# set prefix for distinguishing minimal from develop image (used by the 'set-docker-image-info' script)
+IMAGE_NAME=${1:-"wft4galaxy"}
+
 # absolute path of the current script
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# set prefix for distinguishing minimal from develop image (used by the 'set-docker-image-info' script)
-export IMAGE_TAG_PREFIX="minimal"
-
 # set git && image info
 source ${script_path}/../set-git-repo-info.sh "$@"
-source ${script_path}/../set-docker-image-info.sh
+source ${script_path}/../set-docker-image-info.sh ${IMAGE_NAME}
 
 # Need to cd into this script's directory because image-config assumes it's running within it
 cd "${script_path}"
 
-# override just for debugs
-IMAGE="kikkomep/wft4galaxy-minimal:minimal-travis-integration"
-
 # build the Docker image
 docker build --build-arg git_branch=${GIT_BRANCH} --build-arg git_url=${GIT_HTTPS} -t ${IMAGE} .
-
-# print list of Docker images available
-docker images
 
 # restore the original path
 cd -
