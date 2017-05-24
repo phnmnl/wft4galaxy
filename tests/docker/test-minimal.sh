6355d83a8fefa8f3a23446c332a8631610d545f2
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-24 17:05:32 +0200

    Generalize test-image script

--- tests/docker/test-minimal.sh
+++ tests/docker/test-minimal.sh
@@ -1,28 +1,113 @@
 #!/usr/bin/env bash
 
-# FIXME: check path
-IMAGE_ROOT_PATH=${1:-"utils/docker/minimal"}
-# TODO: implement a better arg parsing
-GALAXY_URL=$2
-GALAXY_API_KEY=$3
-GALAXY_NETWORK=$4
+set -e
+
+# absolute path of the current script
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# base path for Docker images
+image_root_path="${script_path}/../../utils/docker"
+
+# print help
+function print_usage(){
+    (   echo "USAGE: $0 [--server URL] [--api-key API-KEY] [--network ADDRESS] "
+        echo "          [-r|--image-registry REGISTRY] [-o|--image-owner OWNER] [-n|--image-name NAME] [-t|--image-tag TAG]"
+        echo "          {minimal,develop}"
+        echo ""
+        echo "If no options related to the Docker image are provided, this script will try to get the"
+        echo "required image information from the local repository itself") >&2
+}
+
+# parse args
+while test $# -gt 0
+do
+    case "$1" in
+        -h|--help)
+            print_usage
+            exit 0
+            ;;
+        --server )
+            GALAXY_URL=$2
+            shift
+            ;;
+        --api-key)
+            GALAXY_API_KEY=$2
+            shift
+            ;;
+        --network )
+            GALAXY_NETWORK=$2
+            shift
+            ;;
+        -r|--image-registry)
+            export IMAGE_REGISTRY=$2
+            shift
+            ;;
+        -o|--image-owner)
+            export IMAGE_OWNER=$2
+            shift
+            ;;
+        -n|--image-name)
+            export IMAGE_NAME=$2
+            shift
+            ;;
+        -t|--image-tag)
+            export IMAGE_TAG=$2
+            shift
+            ;;
+        --*)
+            print_usage
+            exit -1
+            ;;
+        *)
+            # support only the first argument; skip all remaining
+            if [[ -z ${image_type} ]]; then
+                image_type=${1}
+            fi
+            ;;
+    esac
+    shift
+done
+
+# check required options
+settings=(image_type GALAXY_URL GALAXY_API_KEY)
+for s in ${settings[@]}; do
+    if [[ -z ${!s} ]]; then
+        echo "No ${s} provided."
+        exit -1
+    fi
+done
 
 # download wft4galaxy script
 curl -s https://raw.githubusercontent.com/phnmnl/wft4galaxy/develop/utils/docker/install.sh | bash /dev/stdin .
 
 # switch the Docker image context
-cd ${IMAGE_ROOT_PATH}
+cd ${image_root_path}
 
 # build docker image
-./build-remote.sh --branch ${TRAVIS_BRANCH} && cd -
+"${image_root_path}/${image_type}/build.sh" && cd -
 
-# FIXME: remove... it is just for debug
-docker run --rm --network galaxy \
-            ubuntu bash -c "apt-get update && apt-get install -y iputils-ping && timeout 10 ping 172.18.0.22"
+# set optional arguments
+cmd_other_opts="--skip-update"
+if [[ -n ${IMAGE_REPOSITORY} ]]; then
+    cmd_other_opts="${cmd_other_opts} --repository ${IMAGE_REPOSITORY}"
+fi
+if [[ -n ${IMAGE_TAG} ]]; then
+    # TODO: update version to tag
+    cmd_other_opts="${cmd_other_opts} --version ${IMAGE_TAG}"
+fi
+if [[ -n ${GALAXY_NETWORK} ]]; then
+    cmd_other_opts="${cmd_other_opts} --network ${GALAXY_NETWORK}"
+fi
 
+# uncomment for debug
+#echo "Trying to contact Galaxy sever..."
+#docker run --rm --network ${GALAXY_NETWORK} \
+#            ubuntu bash -c "apt-get update && apt-get install -y iputils-ping && timeout 5 ping 172.18.0.22"
 
-# Run examples  # FIXME: change the repo and version TAGs
-wft4galaxy-docker --skip-update --repository kikkomep --version minimal-travis-integration \
-                  --network ${GALAXY_NETWORK} --debug \
-                  --server ${GALAXY_URL} --api-key ${GALAXY_API_KEY} \
-                  -f examples/change_case/workflow-test.yml
+# build cmd
+base_cmd="wft4galaxy-docker --debug ${cmd_other_opts} --server ${GALAXY_URL} --api-key ${GALAXY_API_KEY}"
+cmd="${base_cmd} -f examples/change_case/workflow-test.yml"
+echo "CMD: ${cmd}">&2
+
+# run test
+${cmd}
