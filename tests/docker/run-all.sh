6ce1dd86a406af27822db04e470a7f057fdef024
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-24 17:04:28 +0200

    Update Travis configuration to use the new docker/run-all script

---
+++ tests/docker/run-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# absolute path of the current script
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# test minimal image
+${script_path}/test-image.sh minimal "$@"
+
+# test develop image
+#${script_path}/test-image.sh develop "$@"
