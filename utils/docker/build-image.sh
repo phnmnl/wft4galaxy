5868af62a7a86bf7e74519d07e96677efdba5c32
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-24 12:10:10 +0200

    Add support for customising Docker image name

--- utils/docker/build-image.sh
+++ utils/docker/build-image.sh
@@ -1,17 +1,64 @@
 #!/usr/bin/env bash
 
-# set prefix for distinguishing minimal from develop image (used by the 'set-docker-image-info' script)
-IMAGE_NAME=${1:-"wft4galaxy"}
+
+function print_usage(){
+    (   echo "USAGE: $0 [-o|--image-owner OWNER] [-n|--image-name NAME] [-t|--image-tag TAG] < minimal | develop >"
+        echo
+        echo "If no arguments are provided, this script will try to get the"
+        echo "required repository information from the local repository itself") >&2
+}
+
+# parse args
+while test $# -gt 0
+do
+    case "$1" in
+        -h|--help)
+            print_usage
+            exit 0
+            ;;
+        -r|--image-registry)
+            export IMAGE_REGISTRY=$2
+            shift
+            ;;
+        -o|--image-owner)
+            export IMAGE_OWNER=$2
+            shift
+            ;;
+        -n|--image-name)
+            export IMAGE_NAME=$2
+            shift
+            ;;
+        -t|--image-tag)
+            export IMAGE_TAG=$2
+            shift
+            ;;
+        --*)
+            print_usage
+            exit -1
+            ;;
+        *)
+            ;;
+    esac
+    shift
+done
+
+if [[ -z ${image_type} ]]; then
+    image_type="minimal"
+    echo "No image type provided. Using the default : ${image_type}"
+fi
 
 # absolute path of the current script
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# absolute path of the image folder
+image_path="${script_path}/${image_type}"
+
 # set git && image info
-source ${script_path}/../set-git-repo-info.sh "$@"
-source ${script_path}/../set-docker-image-info.sh ${IMAGE_NAME}
+source ${script_path}/set-git-repo-info.sh "$@"
+source ${script_path}/set-docker-image-info.sh
 
 # Need to cd into this script's directory because image-config assumes it's running within it
-cd "${script_path}"
+cd "${image_path}"
 
 # build the Docker image
 docker build --build-arg git_branch=${GIT_BRANCH} --build-arg git_url=${GIT_HTTPS} -t ${IMAGE} .
--- utils/docker/set-docker-image-info.sh
+++ utils/docker/set-docker-image-info.sh
@@ -1,66 +1,102 @@
 #!/usr/bin/env bash
 
-# set IMAGE_NAME
-IMAGE_NAME=${1}
-
 # parse existing IMAGE
 if [[ -n ${IMAGE} ]]; then
-    export IMAGE_OWNER=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\1/")
-    export IMAGE_NAME=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\2/")
-    export IMAGE_TAG=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\3/")
-fi
 
-# parse existing IMAGE_REPOSITORY
-if [[ -n ${IMAGE_REPOSITORY} ]]; then
-    export IMAGE_OWNER=$(echo ${IMAGE_REPOSITORY} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\1/")
-    export IMAGE_NAME=$(echo ${IMAGE_REPOSITORY} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\2/")
-fi
+    echo "Using IMAGE=${IMAGE} to generate image info...">&2
+    prefixes=$(echo ${IMAGE} | grep -o "/" | wc -l |  sed -e 's/^[[:space:]]*//')
 
-
-# set image owner
-if [[ -z ${IMAGE_OWNER} ]]; then
-    if [[ -z ${GIT_OWNER} ]]; then
-        echo "GIT_OWNER not found">&2
-        exit -1
+    if [[ $prefixes -eq 0 ]]; then
+        export IMAGE_NAME=$(echo ${IMAGE} | sed -E "s/(.*):(.*)(\.git)/\1/")
+        export IMAGE_TAG=$(echo ${IMAGE} | sed -E "s/(.*):(.*)(\.git)/\2/")
+        export IMAGE_OWNER=""
+        export IMAGE_REGISTRY=""
+    elif [[ $prefixes -eq 1 ]]; then
+        export IMAGE_REGISTRY=""
+        export IMAGE_OWNER=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\1/")
+        export IMAGE_NAME=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\2/")
+        export IMAGE_TAG=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\3/")
+    elif [[ $prefixes -eq 2 ]]; then
+        export IMAGE_REGISTRY=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*)\/(.*):(.*)(\.git)/\1/")
+        export IMAGE_OWNER=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*)\/(.*):(.*)(\.git)/\2/")
+        export IMAGE_NAME=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*)\/(.*):(.*)(\.git)/\3/")
+        export IMAGE_TAG=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*)\/(.*):(.*)(\.git)/\4/")
     fi
 
-    # map the git phnmnl repository to the Crs4 DockerHub repository
-    if [[ ${GIT_OWNER} == "phnmnl" ]]; then
-        GIT_OWNER="crs4"
+elif [[ -n ${IMAGE_REPOSITORY} ]]; then
+
+    echo "Using IMAGE_REPOSITORY=${IMAGE_REPOSITORY} to generate image info...">&2
+    prefixes=$(echo ${IMAGE_REPOSITORY} | grep -o "/" | wc -l |  sed -e 's/^[[:space:]]*//')
+
+    export IMAGE_REGISTRY=""
+    if [[ $prefixes -eq 0 ]]; then
+        export IMAGE_NAME=$(echo ${IMAGE} | sed -E "s/(.*):(.*)(\.git)/\1/")
+        export IMAGE_TAG=$(echo ${IMAGE} | sed -E "s/(.*):(.*)(\.git)/\2/")
+        export IMAGE_OWNER=""
+        export IMAGE_REGISTRY=""
+    elif [[ $prefixes -eq 1 ]]; then
+        export IMAGE_REGISTRY=""
+        export IMAGE_OWNER=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\1/")
+        export IMAGE_NAME=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\2/")
+        export IMAGE_TAG=$(echo ${IMAGE} | sed -E "s/(.*)\/(.*):(.*)(\.git)/\3/")
     fi
-    export IMAGE_OWNER=${GIT_OWNER}
-fi
 
-# set IMAGE_NAME if not defined
-if [[ -z ${IMAGE_NAME} ]]; then
-    export IMAGE_NAME="wft4galaxy"
-fi
+# if neither IMAGE_NAME or IMAGE_REPOSITORY are setted
+else
+    echo "Using Git repository info to generate image info...">&2
 
-# set image tag
-if [[ -z ${GIT_BRANCH} && -z ${GIT_TAG} ]]; then
-    echo "GIT_BRANCH or GIT_TAG not found">&2
-    exit -1
-fi
-image_tag=${GIT_BRANCH}
-if [[ -n ${GIT_TAG} ]]; then
-    image_tag=${GIT_TAG}
-fi
-export IMAGE_TAG=${image_tag}
+    # set image owner
+    if [[ -z ${IMAGE_OWNER} ]]; then
+        if [[ -z ${GIT_OWNER} ]]; then
+            echo "GIT_OWNER not found">&2
+            exit -1
+        fi
 
-# set image repository
-if [[ -z ${IMAGE_REPOSITORY} ]]; then
-    export IMAGE_REPOSITORY="${IMAGE_OWNER}/${IMAGE_NAME}"
+        # map the git phnmnl repository to the Crs4 DockerHub repository
+        if [[ ${GIT_OWNER} == "phnmnl" ]]; then
+            GIT_OWNER="crs4"
+        fi
+        export IMAGE_OWNER=${GIT_OWNER}
+    fi
+
+    # set IMAGE_NAME if not defined
+    if [[ -z ${IMAGE_NAME} ]]; then
+        export IMAGE_NAME="wft4galaxy"
+    fi
+
+    # set image tag
+    if [[ -z ${IMAGE_TAG} ]]; then
+        if [[ -z ${GIT_BRANCH} && -z ${GIT_TAG} ]]; then
+            echo "GIT_BRANCH or GIT_TAG not found">&2
+            exit -1
+        fi
+        image_tag=${GIT_BRANCH}
+        if [[ -n ${GIT_TAG} ]]; then
+            image_tag=${GIT_TAG}
+        fi
+        export IMAGE_TAG=${image_tag}
+    fi
+
+    # set image repository
+    if [[ -z ${IMAGE_REPOSITORY} ]]; then
+        export IMAGE_REPOSITORY="${IMAGE_OWNER}/${IMAGE_NAME}"
+    fi
 fi
 
 # set image repository
 if [[ -z ${IMAGE} ]]; then
-    export IMAGE="${IMAGE_OWNER}/${IMAGE_NAME}:${IMAGE_TAG}"
+    if [[ -n ${IMAGE_REGISTRY} ]]; then
+        export IMAGE="${IMAGE_REGISTRY}/${IMAGE_OWNER}/${IMAGE_NAME}:${IMAGE_TAG}"
+    else
+        export IMAGE="${IMAGE_OWNER}/${IMAGE_NAME}:${IMAGE_TAG}"
+    fi
 fi
 
-# log git/docker info
+# log Docker image info
 echo " - Docker image: ${IMAGE}" >&2
 echo " - Docker image owner: ${IMAGE_OWNER}" >&2
 echo " - Docker image name: ${IMAGE_NAME}" >&2
 echo " - Docker image tag: ${IMAGE_TAG}" >&2
+echo " - Docker image registry: ${IMAGE_REGISTRY}" >&2
 echo " - Docker image repository: ${IMAGE_REPOSITORY}" >&2
 
