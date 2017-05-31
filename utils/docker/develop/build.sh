03a78239b2b52cbef6c3d985cb0e519807542801
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-31 10:38:54 +0200

    Fix hooks

--- utils/docker/develop/build.sh
+++ utils/docker/develop/build.sh
@@ -1,1 +1,7 @@
-hooks/build
+#!/usr/bin/env bash
+
+# absolute path of the current script
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# call the build script
+${script_path}/../build-image.sh -n "wft4galaxy-develop" "develop" "$@"
