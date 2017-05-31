b0949d40f02f9abeb4c55846d5d069031bd55e25
Marco Enrico Piras <kikkomep@crs4.it>
2017-05-31 10:24:54 +0200

    Restore original “entry point” script of the “develop” image

--- utils/docker/develop/entrypoint.sh
+++ utils/docker/develop/entrypoint.sh
@@ -2,16 +2,20 @@
 
 source entrypoint-argparser.sh "$@"
 
-if [[ ! ${ENTRYPOINT} =~ ^(bash|wft4galaxy|runtest|wizard)$  ]]; then
+if [[ ! ${ENTRYPOINT} =~ ^(bash|ipython|jupyter|wft4galaxy|runtest|wizard)$ ]]; then
     echo -e "\nERROR: Command \"${ENTRYPOINT_ARGS} \" not supported !"
-    echo -e "       Supported commands: bash | wft4galaxy | runtest | wizard \n"
+    echo -e "       Supported commands: bash | ipython| jupyter | wft4galaxy | runtest | wizard \n"
     exit -1
 fi
 
-if [[ ${ENTRYPOINT} == "bash" ]]; then
-    /bin/bash ${ENTRYPOINT_ARGS}
+if [[ ${ENTRYPOINT} == "wft4galaxy" ]] || [[ ${ENTRYPOINT} == "runtest" ]]; then
+    wft4galaxy ${ENTRYPOINT_ARGS}
 elif [[ ${ENTRYPOINT} == "wizard" ]]; then
     wft4galaxy-wizard ${ENTRYPOINT_ARGS}
+elif [[ ${ENTRYPOINT} == "ipython" ]]; then
+	ipython ${ENTRYPOINT_ARGS}
+elif [[ ${ENTRYPOINT} == "jupyter" ]]; then
+	ipython notebook --ip=$(hostname) --no-browser --allow-root ${ENTRYPOINT_ARGS}
 else
-    wft4galaxy ${ENTRYPOINT_ARGS}
+    /bin/bash ${ENTRYPOINT_ARGS}
 fi
