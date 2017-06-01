f91a13fb972c5c33f12f9450ab3f831ad26cd130
Marco Enrico Piras <kikkomep@crs4.it>
2017-06-01 14:14:39 +0200

    Rename scripts for testing comparators

--- tests/comparators/rounded_comparison_csv/test_comparator.py
+++ tests/comparators/rounded_comparison_csv/test_comparator.py
@@ -4,7 +4,8 @@ import os
 import sys
 import unittest
 
-MyDir = os.path.abspath(os.path.dirname(__file__))
+TestDir = os.path.abspath(os.path.dirname(__file__))
+
 
 class TestWft4Galaxy(unittest.TestCase):
     def setUp(self):
@@ -13,10 +14,9 @@ class TestWft4Galaxy(unittest.TestCase):
     def tearDown(self):
         pass
 
-class TestRoundedCsvComparator(unittest.TestCase):
 
-    TestDir = os.path.join(MyDir, 'comparators', 'rounded_comparison_csv')
-    CorrelationFile = os.path.join(TestDir,  'correlation_table.csv')
+class TestRoundedCsvComparator(unittest.TestCase):
+    CorrelationFile = os.path.join(TestDir, 'correlation_table.csv')
 
     def test_identical_files(self):
         from wft4galaxy.comparators import rounded_comparison_csv
@@ -24,21 +24,20 @@ class TestRoundedCsvComparator(unittest.TestCase):
 
     def test_diff_heading(self):
         from wft4galaxy.comparators import rounded_comparison_csv
-        different_file = os.path.join(self.TestDir, 'diff_heading.csv')
+        different_file = os.path.join(TestDir, 'diff_heading.csv')
         self.assertFalse(rounded_comparison_csv(different_file, self.CorrelationFile))
 
     def test_diff_field(self):
         from wft4galaxy.comparators import rounded_comparison_csv
-        different_file = os.path.join(self.TestDir, 'diff_field.csv')
+        different_file = os.path.join(TestDir, 'diff_field.csv')
         self.assertFalse(rounded_comparison_csv(different_file, self.CorrelationFile))
 
     def test_diff_field_less_than_roundoff(self):
         from wft4galaxy.comparators import rounded_comparison_csv
         # A number in a field is different, but at a higher precision than required
         # by comparison function
-        different_file = os.path.join(self.TestDir, 'diff_under_round.csv')
+        different_file = os.path.join(TestDir, 'diff_under_round.csv')
         self.assertTrue(rounded_comparison_csv(different_file, self.CorrelationFile))
-
 
 
 def suite():
@@ -46,9 +45,11 @@ def suite():
     s.addTests(unittest.TestLoader().loadTestsFromTestCase(TestRoundedCsvComparator))
     return s
 
+
 def main():
     result = unittest.TextTestRunner(verbosity=2).run(suite())
     return 0 if result.wasSuccessful() else 1
+
 
 if __name__ == '__main__':
     sys.exit(main())
