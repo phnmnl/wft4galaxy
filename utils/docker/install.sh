6d2fcf004ff72d295e78de290d15a2c46d3e9044
Marco Enrico Piras <kikkomep@crs4.it>
2017-06-08 10:40:14 +0200

    Fix install script

--- utils/docker/install.sh
+++ utils/docker/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -e
+set -o errexit
+set -o nounset
 
 # set default repo owner
 DEFAULT_REPO="phnmnl/wft4galaxy"
@@ -11,6 +12,9 @@ DEFAULT_REPO_BRANCH="master"
 # set default target folder
 DEFAULT_TARGET_FOLDER="/usr/local/bin"
 
+# empty temporary folder
+TEMP_DIR=""
+
 # print usage
 function print_usage(){
     echo -e "\nUSAGE: ${0} [--repo <REPOSITORY>] [--branch <REPOSITORY_BRANCH>] [TARGET_FOLDER]"
@@ -20,13 +24,21 @@ function print_usage(){
     echo -e "\t - TARGET_FOLDER:     the path where to store the wft4galaxy-docker script (default: \"${DEFAULT_TARGET_FOLDER}\")"
 }
 
+# delete temporary files
+function cleanup() {
+	if [[ -n "${TEMP_DIR}" && -d "${TEMP_DIR}" ]]; then
+		rm -rf "${TEMP_DIR}"
+	fi
+}
+
 # set default values
 REPO=${DEFAULT_REPO}
 REPO_BRANCH=${DEFAULT_REPO_BRANCH}
 TARGET_FOLDER=${DEFAULT_TARGET_FOLDER}
 
 # parse arguments
-while [ -n "$1" ]; do
+OTHER_OPTS=''
+while [ $# -gt 0 ]; do
     # Copy so we can modify it (can't modify $1)
     OPT="$1"
     # Detect argument termination
@@ -84,11 +96,15 @@ elif [[ ${#ARGS[@]} -gt 1 ]]; then
     exit 99
 fi
 
+# On exit, for whatever reason, try to clean up
+trap "cleanup" EXIT INT ERR
+
 # set source script
 SOURCE_SCRIPT="https://raw.githubusercontent.com/${REPO}/${REPO_BRANCH}/wft4galaxy/app/docker_runner.py"
 
 # download script
-TEMP_SCRIPT="/tmp/wft4galaxy-docker"
+TEMP_DIR=$(mktemp -d)
+TEMP_SCRIPT="${TEMP_DIR}/wft4galaxy-docker"
 STATUS_CODE=$(curl --silent --output ${TEMP_SCRIPT} --write-out "%{http_code}" ${SOURCE_SCRIPT})
 if [[ ${STATUS_CODE} -ne 200 ]]; then
     echo -e "\nERROR: Script ${SOURCE_SCRIPT} not found!"
